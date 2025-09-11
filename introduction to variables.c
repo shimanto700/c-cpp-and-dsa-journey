d913b63441cbf4f7808636a003ad727a7cb325ae
Hussain Shimanto <hussainshimanto715@gmail.com>
2025-09-11 19:12:12 +0000

    test commit

---
+++ introduction to variables.c
@@ -0,0 +1,21 @@
+// #include <stdio.h>
+// int main() {
+//     int var = 10; // Declare an integer variable 'var' and initialize it to 10
+//     var = 20;     // Reassign the value of 'var' to 20
+//     printf("The value of var is: %d\n", var); // Print the value of 'var'
+  
+//     return 0; // Indicate that the program ended successfully
+// }
+
+// // This program demonstrates the declaration, initialization, reassignment, and printing of a variable in C.
+
+#include <stdio.h>
+int main() {
+   int var1, var2, var3; // Declare multiple integer variables
+   var1 = var2 = var3 = 10; // Initialize multiple variables to the same value
+   printf("The value of var1 is: %d\n", var1); // Print the value of 'var1'
+   printf("The value of var2 is: %d\n", var2); // Print the value of 'var2'
+   printf("The value of var3 is: %d\n", var3); // Print the value of 'var3'
+  
+    return 0; // Indicate that the program ended successfully
+}
